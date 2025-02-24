3dee518c150f988db0351ff5b48eff7b1a585a30
gunhee <rjsgml771@naver.com>
2025-02-24 14:45:01 +0900

    feat: 유저 쿠키 없으면 자동으로 logout

--- advance/javascript/userMenu.js
+++ advance/javascript/userMenu.js
@@ -23,6 +23,8 @@ if (userCookie) {
     console.log(err);
     window.location.href = "index.html";
   }
+} else {
+  window.location.href = "index.html";
 }
 
 logo.addEventListener("click", () => {
