c810565d71d6f2997cfc9ff4f940f4f6fac43a74
gunhee <rjsgml771@naver.com>
2025-02-25 18:46:19 +0900

    feat: 댓글 삭제 수정 로직 추가

--- advance/javascript/post.js
+++ advance/javascript/post.js
@@ -1,6 +1,10 @@
+import { getCookie } from "./userMenu.js";
+
 document.addEventListener("DOMContentLoaded", async function () {
   const urlParams = new URLSearchParams(window.location.search);
-  const postId = urlParams.get("id");
+  const postId = parseInt(urlParams.get("id"));
+  const userCookie = getCookie("user");
+  const user = JSON.parse(userCookie);
 
   function formatLikes(likes) {
     if (likes >= 1000 && likes < 1000000) {
@@ -39,7 +43,92 @@ document.addEventListener("DOMContentLoaded", async function () {
     console.error("Error loading post:", error);
   }
 
-  const editButton = document.getElementById("post-edit-button");
+  try {
+    const response = await fetch("../data/post/comments/comments.json");
+    const data = await response.json();
+    const comments = data.filter((comment) => comment.postId === postId);
+
+    console.log(comments);
+
+    const commentContainer = document.getElementById("comment-container");
+    commentContainer.innerHTML = "";
+
+    comments.map((comment) => {
+      const commentItem = document.createElement("div");
+      commentItem.className = "comment-item";
+      commentItem.setAttribute("id", comment.id);
+
+      commentItem.innerHTML = `
+        <div class="comment-item-header">
+          <img class="author-logo" src=${comment.author_image} alt="logo" />
+          <span class="comment-item-author">${comment.author}</span>
+          <span class="comment-item-date">${comment.createdAt}</span>
+        </div>
+        <div class="comment-item-content">
+          <p id="comment-text-${comment.id}">${comment.content}</p>
+        </div>
+        ${
+          comment.userId === user.id
+            ? `
+            <div class="comment-edit-container">
+              <button id=${comment.id} class="comment-edit-button edit-button">
+                수정
+              </button>
+              <button id=${comment.id} class="comment-delete-button edit-button">
+                삭제
+              </button>
+            </div>
+          `
+            : ``
+        }
+      `;
+      commentContainer.appendChild(commentItem);
+
+      commentContainer.addEventListener("click", function (event) {
+        const target = event.target;
+
+        if (target.classList.contains("comment-delete-button")) {
+          const commentId = target.getAttribute("id");
+          console.log(`댓글 삭제: ${commentId}`);
+
+          deleteModal.style.display = "flex";
+          modalTitle.textContent = "댓글을 삭제하시겠습니까?";
+
+          confirmDeleteButton.onclick = function () {
+            console.log(`삭제된 댓글 ID: ${commentId}`);
+            document.querySelector(`.comment-item[id="${commentId}"]`).remove();
+            deleteModal.style.display = "none";
+          };
+        }
+        if (target.classList.contains("comment-edit-button")) {
+          const commentId = target.getAttribute("id");
+          console.log(`댓글 수정: ${commentId}`);
+
+          const commentText = document.getElementById(
+            `comment-text-${commentId}`
+          );
+
+          commentArea.value = commentText.textContent;
+          commentButton.textContent = "댓글 수정";
+          commentButton.style.backgroundColor = "#aca0e8";
+          commentButton.style.cursor = "pointer";
+
+          commentButton.replaceWith(commentButton.cloneNode(true));
+          commentButton = document.getElementById("comment-button");
+
+          commentButton.onclick = function () {
+            console.log(`수정된 댓글 ID: ${commentId}, ${commentArea.value}`);
+            commentText.textContent = commentArea.value;
+            commentArea.value = "";
+            setCommentButtonDefault();
+          };
+        }
+      });
+    });
+  } catch (error) {
+    console.log("Error Comments:", error);
+  }
+
   const deleteButton = document.getElementById("post-delete-button");
   const deleteModal = document.getElementById("delete-modal");
   const modalTitle = document.getElementById("delete-modal-title");
@@ -48,44 +137,10 @@ document.addEventListener("DOMContentLoaded", async function () {
   const likesItems = document.getElementById("post-likes");
   const likesCount = document.getElementById("post-likes-count");
   const commentArea = document.getElementById("comment");
-  const commentButton = document.getElementById("comment-button");
-  const commentText = document.getElementById("comment-text");
-  const commentEditButton = document.getElementById("comment-edit-button");
-  const commentDeleteButton = document.getElementById("comment-delete-button");
+  let commentButton = document.getElementById("comment-button");
 
   let isLiked = false;
   let likes = parseInt(post.likes);
-
-  function checkComment() {
-    if (commentArea.value.length === 0) {
-      commentButton.style.cursor = "not-allowed";
-      commentButton.style.backgroundColor = "#d9d9d9";
-    } else {
-      commentButton.style.cursor = "pointer";
-      commentButton.style.backgroundColor = "#aca0e8";
-    }
-  }
-  editButton.addEventListener("click", function () {
-    window.location.href = "editPost.html?id=" + postId;
-  });
-
-  commentArea.addEventListener("input", checkComment);
-  checkComment();
-
-  commentButton.addEventListener("click", function () {
-    if (commentButton.textContent === "댓글 등록") {
-      console.log(commentArea.value);
-      commentArea.value = "";
-      commentButton.style.backgroundColor = "#d9d9d9";
-      commentButton.disabled = true;
-    } else {
-      console.log("댓글이 수정되었습니다.");
-      commentText.textContent = commentArea.value;
-      commentArea.value = "";
-      commentButton.style.backgroundColor = "#d9d9d9";
-      commentButton.disabled = true;
-    }
-  });
 
   likesItems.addEventListener("click", function () {
     if (isLiked) {
@@ -122,16 +177,32 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
   });
 
-  commentDeleteButton.addEventListener("click", function () {
-    deleteModal.style.display = "flex";
-    document.getElementById("delete-modal-title").textContent =
-      "댓글을 삭제하시겠습니까?";
-  });
+  setCommentButtonDefault();
 
-  commentEditButton.addEventListener("click", function () {
-    commentArea.textContent = commentText.textContent;
-    commentButton.textContent = "댓글 수정";
-    commentButton.style.backgroundColor = "#aca0e8";
-    commentButton.style.cursor = "pointer";
-  });
+  function setCommentButtonDefault() {
+    commentButton.textContent = "댓글 등록";
+    commentButton.style.backgroundColor = "#d9d9d9";
+    commentButton.style.cursor = "not-allowed";
+
+    commentButton.replaceWith(commentButton.cloneNode(true));
+    commentButton = document.getElementById("comment-button");
+
+    commentButton.onclick = function () {
+      if (commentArea.value.trim() === "") return;
+
+      console.log("새 댓글 등록: ", commentArea.value);
+      commentArea.value = "";
+      setCommentButtonDefault();
+    };
+
+    commentArea.oninput = function () {
+      if (commentArea.value.trim()) {
+        commentButton.style.backgroundColor = "#aca0e8";
+        commentButton.style.cursor = "pointer";
+      } else {
+        commentButton.style.backgroundColor = "#d9d9d9";
+        commentButton.style.cursor = "not-allowed";
+      }
+    };
+  }
 });
