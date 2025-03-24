b9426273f36e401e829a0451f93ced67f74fc585
gunhee <rjsgml771@naver.com>
2025-03-24 13:14:47 +0900

    feat: 유저정보 api 연결

--- advance/javascript/user.js
+++ advance/javascript/user.js
@@ -1,87 +1,106 @@
-import { getCookie } from "../util/cookie.js";
-import { validateNickname } from "../util/validate.js";
+import { deleteUser, getUser, updateUser } from "../api/userApi.js";
+import { navigateTo } from "../util/navigate.js";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const nicknameInput = document.getElementById("nickname");
-  const errorMessage = document.getElementById("error-message");
-  const editButton = document.querySelector(".button");
-  const toast = document.getElementById("toast");
-  const fileInput = document.getElementById("file-input");
-  const profileImage = document.getElementById("profile-image");
-  const changeImageButton = document.getElementById("change-image-button");
-  const submitButton = document.getElementById("submit-button");
-  const deleteUserButton = document.getElementById("delete-user");
-  const deleteModal = document.getElementById("delete-modal");
-  const confirmDeleteButton = document.getElementById("confirm-delete");
-  const cancelDeleteButton = document.getElementById("cancel-delete");
-  const emailText = document.getElementById("email-text");
+const nicknameInput = document.getElementById("nickname");
+const errorMessage = document.getElementById("error-message");
+const editButton = document.querySelector(".button");
+const toast = document.getElementById("toast");
+const fileInput = document.getElementById("file-input");
+const profileImage = document.getElementById("profile-image");
+const changeImageButton = document.getElementById("change-image-button");
+const submitButton = document.getElementById("submit-button");
+const deleteUserButton = document.getElementById("delete-user");
+const deleteModal = document.getElementById("delete-modal");
+const confirmDeleteButton = document.getElementById("confirm-delete");
+const cancelDeleteButton = document.getElementById("cancel-delete");
+const emailText = document.getElementById("email-text");
 
-  const user = getCookie("user");
-  let userInfo;
-  if (user) {
-    try {
-      userInfo = JSON.parse(user);
-      emailText.textContent = userInfo.email;
-      nicknameInput.value = userInfo.nickname;
-      profileImage.src = userInfo.profile_image;
-    } catch (e) {
-      console.log(e);
-      window.location.href = "/index.html";
-    }
+const user = await getUser();
+let userInfo;
+if (user) {
+  try {
+    emailText.textContent = user.email;
+    nicknameInput.value = user.nickname;
+    profileImage.src = userInfo.profile_image;
+  } catch (e) {
+    console.log(e);
   }
+}
 
-  editButton.addEventListener("click", function () {
-    const nicknameValue = nicknameInput.value.trim();
+const submit = async () => {
+  try {
+    const response = await updateUser(
+      nicknameInput.value,
+      profileImage.src.substring(0, 254)
+    );
 
-    if (nicknameValue === "") {
-      errorMessage.textContent = "*닉네임을 입력해주세요";
-    } else if (nicknameValue.length < 2 || nicknameValue.length > 10) {
-      errorMessage.textContent = "*닉네임은 최대 10자 까지 작성 가능합니다.";
-    } else if (validateNickname(nicknameValue)) {
-      errorMessage.textContent = "*중복된 닉네임 입니다.";
-    } else {
-      errorMessage.textContent = "";
-      console.log("닉네임 수정:", nicknameValue);
-
-      toast.style.display = "block";
-      toast.style.opacity = 1;
+    toast.style.display = "block";
+    toast.style.opacity = 1;
+    setTimeout(() => {
+      toast.style.opacity = 0;
       setTimeout(() => {
-        toast.style.opacity = 0;
-        setTimeout(() => {
-          toast.style.display = "none";
-        }, 500);
-      }, 2000);
-    }
-  });
+        toast.style.display = "none";
+      }, 500);
+    }, 2000);
+  } catch (error) {
+    errorMessage.textContent = error.message;
+  }
+};
 
-  submitButton.addEventListener("click", () => {
-    window.location.href = "/page/posts.html";
-  });
+editButton.addEventListener("click", function () {
+  const nicknameValue = nicknameInput.value.trim();
 
-  changeImageButton.addEventListener("click", function () {
-    fileInput.click();
-  });
+  if (nicknameValue === "") {
+    errorMessage.textContent = "*닉네임을 입력해주세요";
+  } else if (nicknameValue.length < 2 || nicknameValue.length > 10) {
+    errorMessage.textContent = "*닉네임은 최대 10자 까지 작성 가능합니다.";
+  } else {
+    errorMessage.textContent = "";
 
-  fileInput.addEventListener("change", function () {
-    const file = fileInput.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        profileImage.src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  });
+    submit();
+  }
+});
 
-  deleteUserButton.addEventListener("click", function () {
-    deleteModal.style.display = "flex";
-  });
+submitButton.addEventListener("click", () => {
+  submit();
+  navigateTo("/posts");
+});
 
-  confirmDeleteButton.addEventListener("click", function () {
-    window.location.href = "../../index.html";
-  });
+changeImageButton.addEventListener("click", function () {
+  fileInput.click();
+});
 
-  cancelDeleteButton.addEventListener("click", function () {
-    deleteModal.style.display = "none";
-  });
+fileInput.addEventListener("change", function () {
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      profileImage.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+deleteUserButton.addEventListener("click", function () {
+  deleteModal.style.display = "flex";
+});
+
+confirmDeleteButton.addEventListener("click", async function () {
+  try {
+    await deleteUser();
+
+    // accessToken, refreshToken 쿠키 삭제
+    document.cookie.split(";").forEach((cookie) => {
+      const [name] = cookie.split("=");
+      document.cookie = `${name.trim()}=; path=/; max-age=0`;
+    });
+
+    navigateTo("/login");
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+cancelDeleteButton.addEventListener("click", function () {
+  deleteModal.style.display = "none";
 });
